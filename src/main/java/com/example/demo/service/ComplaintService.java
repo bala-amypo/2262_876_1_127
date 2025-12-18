f5ba06a6a0a8a1e8e1b33c54806c47d6a4a05b33
bala-amypo <bala@amypo.in>
2025-12-18 08:50:25 +0000

    User commit

--- src/main/java/com/example/demo/service/ComplaintService.java
+++ src/main/java/com/example/demo/service/ComplaintService.java
@@ -3,4 +3,11 @@ package com.example.demo.service;
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.web.bind.annotation.*;
 import java.util.*;
-import 
+
+
+public interface ComplaintService{
+    public User submitComplaint(ComplaintRequest request);
+    public List<Complaint> getUserComplaints(Long userId);
+    public List<Complaint> getPrioritizedComplaints();
+    public void updateComplaintStatus(Long id, String status);
+}
