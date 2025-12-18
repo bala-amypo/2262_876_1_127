1ed31cfd7d54e93969611e67c3f6f737b6174400
bala-amypo <bala@amypo.in>
2025-12-18 09:45:25 +0000

    User commit

--- src/main/java/com/example/demo/controller/PriorityRuleController.java
+++ src/main/java/com/example/demo/controller/PriorityRuleController.java
@@ -3,6 +3,9 @@ package com.example.demo.controller;
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.web.bind.annotation.*;
 import java.util.*;
+import com.example.demo.entity.PriorityRule;
+import com.example.demo.service.PriorityRuleService;
+
 
 @RestController
 @RequestMapping("/rules")
--- src/main/java/com/example/demo/service/PriorityRuleService.java
+++ src/main/java/com/example/demo/service/PriorityRuleService.java
@@ -3,8 +3,8 @@ package com.example.demo.service;
 import com.example.demo.entity.User;
 import com.example.demo.entity.Complaint;
 import java.util.*;
+import com.example.demo.entity.PriorityRule;
 
-public interface PriorityRuleService extends {
-    public List<PriorityRule> getAllRules()
-
+public interface PriorityRuleService {
+    public List<PriorityRule> getAllRules();
 }
