9018db8c96fa3e23590a167e37e9e56ba54243d9
bala-amypo <bala@amypo.in>
2025-12-18 09:55:26 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/PriorityRuleServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/PriorityRuleServiceImpl.java
@@ -3,17 +3,17 @@ package com.example.demo.service.impl;
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.stereotype.*;
 import java.util.*;
-import com.example.demo.repository.ComplaintRepository;
-import com.example.demo.service.ComplaintService;
-import com.example.demo.entity.Complaint;
+import com.example.demo.entity.PriorityRule;
+import com.example.demo.service.PriorityRuleService;
+import com.example.demo.repository.PriorityRuleRepository;
 
 @Service
 public class PriorityRuleServiceImpl implements PriorityRuleService{
 
     @Autowired
-    PriorityRuleServiceRepository repo;
+    PriorityRuleRepository repo;
 
     public List<PriorityRule> getAllRules(){
-        returnrepo.findAll();
+        return repo.findAll();
     }
 }
