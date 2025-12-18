0e2d2ff1f5aefe157d054d5c4728b0419d336c14
bala-amypo <bala@amypo.in>
2025-12-18 05:16:00 +0000

    User commit

--- src/main/java/com/example/demo/repository/ComplaintStatusRepository.java
+++ src/main/java/com/example/demo/repository/ComplaintStatusRepository.java
@@ -3,10 +3,10 @@ package com.example.demo.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.example.demo.entity.PriorityRule;
+import com.example.demo.entity.ComplaintStatus;
 
 @Repository
-public interface PriorityRuleRepository extends JpaRepository<PriorityRule, Integer> {
+public interface ComplaintStatusRepository extends JpaRepository<ComplaintStatus, Integer> {
     
     
 }
