6ea7dc01a91af40dd74ccc321540483b362c883e
bala-amypo <bala@amypo.in>
2025-12-18 09:25:25 +0000

    User commit

--- src/main/java/com/example/demo/controller/ComplaintController.java
+++ src/main/java/com/example/demo/controller/ComplaintController.java
@@ -3,10 +3,18 @@ package com.example.demo.controller;
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.web.bind.annotation.*;
 import java.util.*;
+import com.example.demo.entity.Complaint;
+import com.example.demo.service.ComplaintService;
 
-@RestController("/complaints")
+@RestController
+@RequestMapping("/complaints")
 public class ComplaintController{
 
+    @Autowired
+    ComplaintService service;
+
     @PostMapping("/submit")
-    public Complaint submitComplaint(@RequestBody )
+    public Complaint submitComplaint(@RequestBody Complaint complaint){
+        return service.submitComplaint(complaint);
+    }
 }
