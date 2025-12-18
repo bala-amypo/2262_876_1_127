b2688b6064d6e817fc41b2975b36ae2ff538c9d8
bala-amypo <bala@amypo.in>
2025-12-18 04:32:30 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/PriorityRule.java
@@ -0,0 +1,46 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class User{
+    @Id
+    private long id;
+    private String category, description;
+    private int baseScore;
+
+    public long getId(){
+        return this.id;
+    }
+
+    public long setId(int id){
+        this.id = id;
+    }
+
+
+    public String getName(){
+        return this.name;
+    }
+
+    public setName(String name){
+        this.name = name;
+    }
+
+
+    public String getEmail(){
+        return this.email;
+    }
+
+    public setEmail(String email){
+        this.email = email;
+    }
+
+
+    public String getPassword(){
+        return this.password;
+    }
+
+    public setPassword(String password){
+        this.password = password;
+    }
+}
