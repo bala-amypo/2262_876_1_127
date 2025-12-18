3da4ea270658a8243b94190015a3a48893caa27c
bala-amypo <bala@amypo.in>
2025-12-18 03:48:23 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/PriorityRuleController.java
@@ -0,0 +1,10 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.web.bind.annotation.*;
+import java.util.*;
+
+@RestController
+public class PriorityRuleController{
+    
+}
