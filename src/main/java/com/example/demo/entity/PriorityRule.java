7652b111ba4e896d16ecad098a060b47827cb4e7
bala-amypo <bala@amypo.in>
2025-12-20 18:13:00 +0000

    User commit

--- src/main/java/com/example/demo/entity/PriorityRule.java
+++ src/main/java/com/example/demo/entity/PriorityRule.java
@@ -1,61 +1,39 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.*;
-import io.swagger.v3.oas.annotations.media.Schema;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 
 @Entity
-public class PriorityRule{
+public class PriorityRule {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Schema(hidden = true)
     private Long id;
-    private String category, description;
-    private int baseScore;
 
-    public Long getId(){
-        return this.id;
+    private String ruleName;
+    private int weight;
+    private boolean active;
+
+    public String getRuleName() {
+        return ruleName;
     }
 
-    public void setId(Long id){
-        this.id = id;
+    public void setRuleName(String ruleName) {
+        this.ruleName = ruleName;
     }
 
-
-    public String getCategory(){
-        return this.category;
+    public int getWeight() {
+        return weight;
     }
 
-    public void setCategory(String category){
-        this.category = category;
+    public void setWeight(int weight) {
+        this.weight = weight;
     }
 
-
-    public String getDescription(){
-        return this.description;
+    public boolean isActive() {
+        return active;
     }
 
-    public void setDescription(String description){
-        this.description = description;
-    }
-
-
-    public int getBaseScore(){
-        return this.baseScore;
-    }
-
-    public void setBaseScore(int baseScore){
-        this.baseScore = baseScore;
-    }
-
-    public PriorityRule(String category, String description, int baseScore){
-        this.category = category;
-        this.description = description;
-        this.baseScore = baseScore;
-    }
-    
-    public PriorityRule(){
-        this.baseScore = 10;
+    public void setActive(boolean active) {
+        this.active = active;
     }
 }
