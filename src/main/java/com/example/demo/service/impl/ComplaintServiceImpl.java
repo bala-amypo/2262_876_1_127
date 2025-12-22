3ea4e1c51db7e9519553ffb46c66743fbf5054b3
bala-amypo <bala@amypo.in>
2025-12-22 06:54:23 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/ComplaintServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/ComplaintServiceImpl.java
@@ -33,7 +33,6 @@ public Complaint submitComplaint(Complaint request) {
         throw new IllegalArgumentException("user.id is required");
     }
 
-    request.setId(null);
 
     Long userId = request.getUser().getId();
     User user = userRepo.findById(userId)
