347030efef2280e16bb581c7d0ff2c3fe606c404
bala-amypo <bala@amypo.in>
2025-12-22 06:51:22 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/PriorityRuleServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/PriorityRuleServiceImpl.java
@@ -1,38 +1,22 @@
 package com.example.demo.service.impl;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.stereotype.*;
+import java.util.*;
 import com.example.demo.entity.PriorityRule;
 import com.example.demo.repository.PriorityRuleRepository;
 import com.example.demo.service.PriorityRuleService;
 
+
 @Service
-public class ComplaintServiceImpl implements ComplaintService {
+public class PriorityRuleServiceImpl implements PriorityRuleService {
 
     @Autowired
-    private ComplaintRepository complaintRepo;
-
-    @Autowired
-    private PriorityRuleRepository priorityRuleRepo;
+    PriorityRuleRepository repo;
 
     @Override
-    public Complaint submitComplaint(Complaint request) {
-
-        Complaint saved = complaintRepo.save(request);
-        if (priorityRuleRepo.count() == 0) {
-
-            PriorityRule rule = new PriorityRule();
-            rule.setCategory(saved.getCategory());
-            rule.setDescription("Auto-created rule from complaint submission");
-            rule.setBaseScore(10);
-
-            priorityRuleRepo.save(rule);
-        }
-
-        return saved;
+    public List<PriorityRule> getAllRules() {
+        return repo.findAll();
     }
-}
 
+}
