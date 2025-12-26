951e867be8a00edf8114afebe1227eb90b0e349e
bala-amypo <bala@amypo.in>
2025-12-26 11:54:52 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,27 +1,25 @@
 package com.example.demo.controller;
 
-import com.example.demo.dto.LoginRequest;
-import com.example.demo.dto.RegisterRequest;
-import com.example.demo.service.AuthService;
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.web.bind.annotation.*;
+import java.util.*;
+import com.example.demo.entity.User;
+import com.example.demo.service.UserService;
 
 @RestController
 @RequestMapping("/auth")
-public class AuthController {
+public class AuthController{
 
-    private final AuthService authService;
-
-    public AuthController(AuthService authService) {
-        this.authService = authService;
-    }
+    @Autowired
+    UserService service;
 
     @PostMapping("/register")
-    public ResponseEntity<?> register(@Valid @RequestBody RegisterRequest req) {
-        authService.register(req);
-        return ResponseEntity.ok("Registered successfully");
+    public User registerUser(@RequestBody User user){
+        return service.registerUser(user);
     }
 
     @PostMapping("/login")
-    public ResponseEntity<?> login(@Valid @RequestBody LoginRequest req) {
-        return ResponseEntity.ok(authService.login(req));
+    public User loginuser(@RequestBody User user){
+        return service.loginUser(user);
     }
 }
