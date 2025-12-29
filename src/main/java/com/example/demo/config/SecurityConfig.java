83e8ff10b54491a6b12e18c21e8fbdba42e4ffc9
bala-amypo <bala@amypo.in>
2025-12-29 09:59:12 +0000

    User commit

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -32,6 +32,7 @@ public class SecurityConfig {
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth
                 // Public endpoints
+                .requestMatchers("/auth/**", "https://9152.32procr.amypo.ai/**", "/v3/api-docs/**").permitAll()
                 .requestMatchers("/auth/**", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
                 // Test endpoints for role verification
                 .requestMatchers("/test/user").hasAnyRole("USER", "CUSTOMER", "AGENT", "ADMIN")
